cb04b01936a2e344eaa6c8c879a319bd2033159e
Evandro Eisinger <evandroeisinger@gmail.com>
2016-05-06 10:30:50 -0300

    feat: support for tags on iOS bridge

--- ios/ReactNativeUrbanAirshipIOS.m
+++ ios/ReactNativeUrbanAirshipIOS.m
@@ -4,18 +4,40 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(enable) {
+RCT_EXPORT_METHOD(enableNotification) {
   [UAirship push].userPushNotificationsEnabled = YES;
 }
 
+RCT_EXPORT_METHOD(disableNotification) {
+  [UAirship push].userPushNotificationsEnabled = NO;
+}
+
+RCT_EXPORT_METHOD(addTag:(NSString *)tag) {
+  [[UAirship push] addTag:tag];
+  [[UAirship push] updateRegistration];
+}
+
+RCT_EXPORT_METHOD(removeTag:(NSString *)tag) {
+  [[UAirship push] removeTag:tag];
+  [[UAirship push] updateRegistration];
+}
+
+RCT_EXPORT_METHOD(setTags:(NSArray *)tags) {
+  [UAirship push].tags = tags;
+  [[UAirship push] updateRegistration];
+}
+
+- (dispatch_queue_t)methodQueue
+{
+  return dispatch_get_main_queue();
+}
+
 + (void)setupUrbanAirship {
-  // Log level
   [UAirship setLogLevel:UALogLevelTrace];
   
   // Load config from AirshipConfig.plist
   UAConfig *config = [UAConfig defaultConfig];
   
-  // Call takeOff (which creates the UAirship singleton)
   [UAirship takeOff:config];
 }
 
