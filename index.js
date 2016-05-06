40322fb787e8863e16011800853112f6ad5f34b0
Evandro Eisinger <evandroeisinger@gmail.com>
2016-05-06 10:26:17 -0300

    feat: support for set, add and remove tags

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 import {
+  NativeAppEventEmitter,
   NativeModules,
   Platform
 } from 'react-native';
 
-const ReactNativeUrbanAirshipIOS = NativeModules.ReactNativeUrbanAirshipIOS;
 
+const ReactNativeUrbanAirshipIOS = NativeModules.ReactNativeUrbanAirshipIOS;
 
 export default class ReactNativeUrbanAirship {
 
-    static enable() {
-      ReactNativeUrbanAirshipIOS.enable();
+    static enable_notification () {
+        ReactNativeUrbanAirshipIOS.enableNotification();
+    }
+
+    static disable_notification () {
+        ReactNativeUrbanAirshipIOS.disableNotification();
+    }
+
+    static add_tag (tag) {
+        ReactNativeUrbanAirshipIOS.addTag(tag);
+    }
+
+    static remove_tag (tag) {
+        ReactNativeUrbanAirshipIOS.removeTag(tag);
+    }
+
+    static set_tags (tags) {
+        ReactNativeUrbanAirshipIOS.setTags(tags);
     }
 
 }
