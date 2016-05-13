dcd36bbc0eac81d8ced93f2c3dd4ad824df1f8e3
Lucas Stephanou <lucas@lucas-ts.com>
2016-05-13 10:07:18 -0300

    native receiver

---
+++ android/src/main/java/com/globo/reactnativeua/ReactNativeUAReceiver.java
@@ -0,0 +1,66 @@
+package com.urbanairship.sample;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import com.urbanairship.AirshipReceiver;
+import com.urbanairship.push.PushMessage;
+
+public class ReactNativeUAReceiver extends AirshipReceiver {
+
+    private static final String TAG = "ReactNativeUAReceiver";
+
+    // private void sendEvent(ReactContext reactContext,
+    //                    String eventName,
+    //                    @Nullable WritableMap params) {
+    //     reactContext
+    //         .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+    //         .emit(eventName, params);
+    // }
+
+    @Override
+    protected void onChannelRegistrationSucceeded(Context context, String channelId) {
+        Log.i(TAG, "Channel registration updated. Channel Id:" + channelId + ".");
+    }
+
+    @Override
+    protected void onChannelRegistrationFailed(Context context) {
+        Log.i(TAG, "Channel registration failed.");
+    }
+
+    @Override
+    protected void onPushReceived(@NonNull Context context, @NonNull PushMessage message, boolean notificationPosted) {
+        // sendEvent(context, "receivedNotification");
+        Log.i(TAG, "Received push message. Alert: " + message.getAlert() + ". posted notification: " + notificationPosted);
+    }
+
+    @Override
+    protected void onNotificationPosted(@NonNull Context context, @NonNull NotificationInfo notificationInfo) {
+        Log.i(TAG, "Notification posted. Alert: " + notificationInfo.getMessage().getAlert() + ". NotificationId: " + notificationInfo.getNotificationId());
+    }
+
+    @Override
+    protected boolean onNotificationOpened(@NonNull Context context, @NonNull NotificationInfo notificationInfo) {
+        Log.i(TAG, "Notification opened. Alert: " + notificationInfo.getMessage().getAlert() + ". NotificationId: " + notificationInfo.getNotificationId());
+
+        // Return false here to allow Urban Airship to auto launch the launcher activity
+        return false;
+    }
+
+    @Override
+    protected boolean onNotificationOpened(@NonNull Context context, @NonNull NotificationInfo notificationInfo, @NonNull ActionButtonInfo actionButtonInfo) {
+        Log.i(TAG, "Notification action button opened. Button ID: " + actionButtonInfo.getButtonId() + ". NotificationId: " + notificationInfo.getNotificationId());
+
+        // Return false here to allow Urban Airship to auto launch the launcher
+        // activity for foreground notification action buttons
+        return false;
+    }
+
+    @Override
+    protected void onNotificationDismissed(@NonNull Context context, @NonNull NotificationInfo notificationInfo) {
+        Log.i(TAG, "Notification dismissed. Alert: " + notificationInfo.getMessage().getAlert() + ". Notification ID: " + notificationInfo.getNotificationId());
+    }
+}
