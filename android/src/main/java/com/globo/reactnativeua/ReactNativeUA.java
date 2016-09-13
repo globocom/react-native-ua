cc85f7d286433503123a78cfbf44ce18cded8cea
Bayron Quinelato <bayron.quinelato@corp.globo.com>
2016-09-13 17:11:29 -0300

    locally custom notif icon in Android and refactored using application

--- android/src/main/java/com/globo/reactnativeua/ReactNativeUA.java
+++ android/src/main/java/com/globo/reactnativeua/ReactNativeUA.java
@@ -2,7 +2,6 @@ package com.globo.reactnativeua;
 
 import android.Manifest;
 import android.app.Activity;
-import android.app.Application;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.NonNull;
@@ -12,7 +11,6 @@ import android.util.Log;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-
 import com.urbanairship.Autopilot;
 import com.urbanairship.UAirship;
 import com.urbanairship.actions.Action;
@@ -21,14 +19,18 @@ import com.urbanairship.actions.ActionCompletionCallback;
 import com.urbanairship.actions.ActionResult;
 import com.urbanairship.actions.ActionRunRequest;
 import com.urbanairship.actions.ActionValue;
+import com.urbanairship.push.notifications.DefaultNotificationFactory;
+import com.urbanairship.push.notifications.NotificationFactory;
 
 
 public class ReactNativeUA extends ReactContextBaseJavaModule {
 
-    public ReactNativeUA(ReactApplicationContext reactContext, Application application) {
+    public ReactNativeUA(final ReactApplicationContext reactContext) {
         super(reactContext);
 
-        Autopilot.automaticTakeOff(application.getApplicationContext());
+        Autopilot.automaticTakeOff(getReactApplicationContext());
+
+        setIcon("ic_notification");
     }
 
     @Override
@@ -84,17 +86,16 @@ public class ReactNativeUA extends ReactContextBaseJavaModule {
     @ReactMethod
     public void handleBackgroundNotification() {
         Activity activity = getCurrentActivity();
-
-        if (activity != null) ReactNativeUAReceiverHelper.setup(getCurrentActivity().getApplicationContext()).sendPushIntent();
+        if (activity != null) {
+            ReactNativeUAReceiverHelper.setup(getCurrentActivity().getApplicationContext()).sendPushIntent();
+        }
     }
 
     @ReactMethod
     public void enableActionUrl() {
         Activity activity = getCurrentActivity();
-
         if (activity != null) {
             ReactNativeUAReceiverHelper.setup(getCurrentActivity().getApplicationContext()).setActionUrl(true);
-
             Log.d("ActionUrl", "Enable default action url behaviour -> True");
         }
     }
@@ -102,10 +103,8 @@ public class ReactNativeUA extends ReactContextBaseJavaModule {
     @ReactMethod
     public void disableActionUrl() {
         Activity activity = getCurrentActivity();
-
         if (activity != null) {
             ReactNativeUAReceiverHelper.setup(getCurrentActivity().getApplicationContext()).setActionUrl(false);
-
             Log.d("ActionUrl", "Disable default action url behaviour -> False");
         }
     }
@@ -116,7 +115,6 @@ public class ReactNativeUA extends ReactContextBaseJavaModule {
 
         if (activity != null) {
             if (shouldRequestPermissions(activity)) {
-
                 ActionRunRequest.createRequest(new Action() {
                     @NonNull
                     @Override
@@ -124,28 +122,26 @@ public class ReactNativeUA extends ReactContextBaseJavaModule {
                         int[] result = requestPermissions(Manifest.permission.ACCESS_COARSE_LOCATION,
                                 Manifest.permission.ACCESS_FINE_LOCATION);
 
-                        for (int i = 0; i < result.length; i++)
-                            if (result[i] == PackageManager.PERMISSION_GRANTED)
+                        for (int i = 0; i < result.length; i++) {
+                            if (result[i] == PackageManager.PERMISSION_GRANTED) {
                                 return ActionResult.newResult(ActionValue.wrap(true));
-
+                            }
+                        }
                         return ActionResult.newResult(ActionValue.wrap(false));
                     }
                 }).run(new ActionCompletionCallback() {
                     @Override
                     public void onFinish(ActionArguments arguments, ActionResult result) {
-
-                        if (result.getValue().getBoolean(false))
+                        if (result.getValue().getBoolean(false)) {
                             UAirship.shared().getLocationManager().setLocationUpdatesEnabled(true);
+                        }
                     }
                 });
 
             } else {
-
                 UAirship.shared().getLocationManager().setLocationUpdatesEnabled(true);
-
             }
         }
-
     }
 
     private boolean shouldRequestPermissions(Activity activity) {
@@ -156,4 +152,51 @@ public class ReactNativeUA extends ReactContextBaseJavaModule {
 
         return corseLocation == PackageManager.PERMISSION_DENIED && fineLocation == PackageManager.PERMISSION_DENIED;
     }
+
+    @ReactMethod
+    public void setIcon(String iconName) {
+        int iconId = getImageResourceId(iconName);
+        if (iconId > 0) {
+            DefaultNotificationFactory defaultNotifFactory = getDefaultNotificationFactory();
+            defaultNotifFactory.setSmallIconId(iconId);
+            UAirship.shared().getPushManager().setNotificationFactory(defaultNotifFactory);
+        }
+    }
+
+    @ReactMethod
+    public void setLargeIcon(String iconName) {
+        int iconId = getImageResourceId(iconName);
+        if (iconId > 0) {
+            DefaultNotificationFactory defaultNotifFactory = getDefaultNotificationFactory();
+            defaultNotifFactory.setLargeIcon(iconId);
+            UAirship.shared().getPushManager().setNotificationFactory(defaultNotifFactory);
+        }
+    }
+
+    private DefaultNotificationFactory getDefaultNotificationFactory() {
+        final NotificationFactory notifFactory = UAirship.shared().getPushManager().getNotificationFactory();
+        if (notifFactory instanceof DefaultNotificationFactory) {
+            return (DefaultNotificationFactory) notifFactory;
+        }
+        return new DefaultNotificationFactory(getReactApplicationContext());
+    }
+
+    private int getImageResourceId(String imageName) {
+        if (imageName == null || imageName.length() <= 0) {
+            return -1;
+        }
+        int imageId = getImageResourceId(imageName, "drawable");
+        if (imageId <= 0) {
+            imageId = getImageResourceId(imageName, "mipmap");
+        }
+        return imageId;
+    }
+
+    private int getImageResourceId(String imageName, String imageResourceType) {
+        return getReactApplicationContext().getResources().getIdentifier(
+                imageName,
+                imageResourceType,
+                getReactApplicationContext().getPackageName());
+    }
+
 }
