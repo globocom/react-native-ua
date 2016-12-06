fcfb085e58478f28809ecdf7a4817e76be4bbcbf
Guilherme Bruzzi <guibruzzi@gmail.com>
2016-12-06 16:55:59 -0200

    Fix #32 : Add public dispatchNotificationEvent to ios implementation and explaining on README how to know on ios when the user accepted the notification.
    Update documentation

--- ios/ReactNativeUAIOS/ReactNativeUAIOS.h
+++ ios/ReactNativeUAIOS/ReactNativeUAIOS.h
@@ -4,6 +4,7 @@
 @interface ReactNativeUAIOS : NSObject <RCTBridgeModule>
 + (void)setupUrbanAirship:(NSDictionary *) launchOptions;
 + (void)setupUrbanAirship:(NSDictionary *) launchOptions withConfig:(UAConfig *) config;
++ (void)dispatchNotificationEvent:(NSString *)eventType notificationData:(NSDictionary *)notificationData;
 @end
 
 @interface PushHandler : NSObject <UAPushNotificationDelegate>
