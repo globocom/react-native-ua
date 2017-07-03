76f4912627a410d5f0c5a07d8598a734a9beba4e
Daniel <daniel.barboza@corp.globo.com>
2017-07-03 17:07:51 -0300

    Fixing handle push notification while App in foreground

--- ios/ReactNativeUAIOS/ReactNativeUAIOS.h
+++ ios/ReactNativeUAIOS/ReactNativeUAIOS.h
@@ -1,9 +1,11 @@
+#import <React/RCTEventEmitter.h>
 #import <React/RCTBridgeModule.h>
 #import "UAPush.h"
 
-@interface ReactNativeUAIOS : NSObject <RCTBridgeModule>
+@interface ReactNativeUAIOS : RCTEventEmitter <RCTBridgeModule>
 + (void)setupUrbanAirship:(NSDictionary *) launchOptions;
 + (void)setupUrbanAirship:(NSDictionary *) launchOptions withConfig:(UAConfig *) config;
++ (void)dispatchNotificationEvent:(NSString *)eventType notificationData:(NSDictionary *)notificationData;
 @end
 
 @interface PushHandler : NSObject <UAPushNotificationDelegate>
